df446f5928b2b8365cb161612884811333eec1f4
priy779 <priyapoovathali@gmail.com>
2025-01-15 23:27:46 -0500

    lesson01

---
+++ lesson01/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.neotech.lesson01;
+
+public class HelloWorld {
+
+	//first java lesson
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("          how r u?");
+		
+
+	}
+
+}
---
+++ lesson01/TestMain.java
@@ -0,0 +1,12 @@
+package com.neotech.lesson01;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		System.out.print("This is the first line.");//print will display the whole statement without jumping to a new line.
+		System.out.println("  This is the second line");
+		System.out.println("This is third line");
+
+	}
+
+}
